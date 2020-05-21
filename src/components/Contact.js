c900dd210d940d0264d883bc0d61d85b87449e4a
Van Bui <van.bt@trans-cosmos.com.vn>
2020-05-21 15:15:58 +0700

    [Route-dom] Prompt - check chuyen trang 3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,17 +6,23 @@ export default class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isChecked: true
+      isChecked: false
     }
   }
 
+  //Prompt nếu trường hợp when thì show thông báo
+  onClick = (isChecked) => {
+    this.setState({
+      isChecked: isChecked
+    })
+  }
   render() {
     var { isChecked } = this.state;
     return (
       <div>
         Đây là trang liên hệ<br />
-        <button type="button" className="btn btn-primary">Cho phép</button><br />
-        <button type="button" className="btn btn-primary">Không cho phép</button>
+        <button type="button" className="btn btn-primary" onClick={() => this.onClick(false)}>Cho phép</button><br />
+        <button type="button" className="btn btn-warning" onClick={() => this.onClick(true)}>Không cho phép</button>
         <Prompt
           when={isChecked}
           message={location => (`Bạn chắc chắn muốn đi đến ${location.pathname}`)}
