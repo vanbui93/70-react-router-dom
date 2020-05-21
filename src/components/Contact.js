0eb4f8b2f4cedb51d321c095b44d4389d85c6cb0
Van Bui <van.bt@trans-cosmos.com.vn>
2020-05-21 15:07:53 +0700

    [Route-dom] Prompt - check chuyen trang

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Prompt} from 'react-router-dom';
 
 export default class Contact extends Component {
   render() {
     return (
       <div>
-        Đây là trang liên hệ
+        Đây là trang liên hệ<br/>
+        <button type="button" className="btn btn-primary">Cho phép</button><br/>
+        <button type="button" className="btn btn-primary">Không cho phép</button>
+        <Prompt
+          when={true}
+          message={location => (`Bạn chắc chắn muốn đi đến ${location.pathname}`)}
+        />
       </div>
     )
   }
